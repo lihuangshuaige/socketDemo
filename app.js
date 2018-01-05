18042c90e88d38aaaf337d8ba5b1c4dcaab2a99b
李煌 <dukeli@qqdyw.net>
2018-01-05 09:50:02 +0800

    修改html

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var express = require("express");
-var a=1;
+var hbs = require("hbs");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var qrcode = require("qrcode-npm");
+var uuid = require('node-uuid');
+var port = 2018;
+http.listen(port, function () {
+    console.log("http://localhost:" + port);
+});
+app.use("/static", express.static(__dirname + "/public"));
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.set('views', __dirname + '/views');
+app.get("/", function (req, res) {
+    res.render("a");
+});
